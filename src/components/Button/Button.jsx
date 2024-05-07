11f019fcccb1965667c637b42ee694b602815a07
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-05-07 19:00:43 +0530

    Refactor Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,7 @@
 import styles from "./Button.module.css";
 
-export default function Button({ variant, children }) {
+export default function Button({ variant, children, isDisabled }) {
   const btnStyles = variant === "primary" ? styles.primary : styles.secondary;
-  return <button className={btnStyles}>{children}</button>;
+  console.log(isDisabled)
+  return <button className={btnStyles} disabled={isDisabled}>{children}</button>;
 }
