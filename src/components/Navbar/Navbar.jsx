af95ea32e6659b75a74668bde9e9edb42ce9602d
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-05-07 20:15:21 +0530

    Refactor Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,31 @@
 import BrandLogo from "../../assets/svg/brand.svg";
+import CrossLogo from "../../assets/svg/cross.svg";
 import styles from "./Navbar.module.css";
 
 import Button from "../Button/Button";
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
+export default function Navbar({ registeration }) {
   return (
-    <nav className={styles.nav}>
+    <nav className={styles.nav} style={{border:registeration?"none":"1px solid #EAEAEA"}}>
       <img src={BrandLogo} alt="Brunel" />
-      <div className="btn-container">
-        <Link to="/register">
-          <Button variant="secondary">Get projects</Button>
-        </Link>
-        <Button variant="primary">Onboard Talent</Button>
-      </div>
+
+      {registeration ? (
+        registeration.register_form ? (
+          <Link to="/">
+            <img src={CrossLogo} alt="" />
+          </Link>
+        ) : (
+          <></>
+        )
+      ) : (
+        <div className={styles.btn_container}>
+          <Link to="/register">
+            <Button variant="secondary">Get projects</Button>
+          </Link>
+          <Button variant="primary">Onboard Talent</Button>
+        </div>
+      )}
     </nav>
   );
 }
