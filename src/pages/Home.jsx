36f3885b7cfb5d9bd1d73a00ceb65ac1a028884d
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-05-07 19:01:03 +0530

    Add Home Page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import FAQ from "../components/FAQ/FAQ.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+import Header from "../components/Header/Header.jsx";
+import Navbar from "../components/Navbar/Navbar.jsx";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <FAQ />
+      <Footer />
+    </>
+  )
+}
