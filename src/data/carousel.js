bf43fd2e572e9ba8e25b29c86e9b73e2732872d3
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-05-07 17:58:10 +0530

    Add demo data for faq and carousel

---
+++ src/data/carousel.js
@@ -0,0 +1,14 @@
+export const carouselData = [
+    {
+        id:crypto.randomUUID(),
+        text:"Enhance fortune 50 company’s insights teams research capabilities"
+    },
+    {
+        id:crypto.randomUUID(),
+        text:"Transforming workforce deployment and project efficiency to drive cost savings and growth."
+    },
+    {
+        id:crypto.randomUUID(),
+        text:"Maximizing productivity and minimizing costs through strategic workforce optimization."
+    },
+]
---
+++ src/data/faq.js
@@ -0,0 +1,27 @@
+export const faqData = [
+  {
+    id: crypto.randomUUID(),
+    question: "Do you offer freelancers?",
+    answer: "Yes, we provide access to a diverse pool of skilled freelancers across various industries and expertise areas.",
+  },
+  {
+    id: crypto.randomUUID(),
+    question: "What’s the guarantee that I will be satisfied with the hired talent?",
+    answer: "Our platform prioritizes client satisfaction. If any issues arise, we facilitate communication between you and the freelancer, offer revisions, and provide support throughout the process.",
+  },
+  {
+    id: crypto.randomUUID(),
+    question: "Can I hire multiple talents at once?",
+    answer: "If unhappy with a project, communicate with the freelancer, allow for revisions, and refer to the agreement. Escalate to platform support if needed, considering mediation. Review policies, seek collaborative solutions for resolution.",
+  },
+  {
+    id: crypto.randomUUID(),
+    question: "Why should I not go to an agency directly?",
+    answer: "While agencies offer a centralized approach, freelancers often bring specialized skills, cost-effectiveness, and personalized attention to your project, ensuring tailored solutions and flexibility.",
+  },
+  {
+    id: crypto.randomUUID(),
+    question: "Who can help me pick a right skillset and duration for me?",
+    answer: "Our platform offers guidance and support in selecting the appropriate skillset and duration for your project. Our dedicated team and resources are available to assist you every step of the way, ensuring optimal outcomes.",
+  },
+];
