d619a111dc79f62a4e64e0c5e32d3a48fcf1b17f
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-05-08 21:48:08 +0530

    Add Page Transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import { Route, Routes, useLocation } from "react-router";
-
+import { AnimatePresence, motion } from "framer-motion";
 import Home from "./pages/Home";
 import Registration from "./pages/Registration";
 
 export default function App() {
   const location = useLocation();
   return (
-    <div>
-      <Routes location={location} key={location.pathname}>
-        <Route index element={<Home />} />
-        <Route path="/register" element={<Registration />} />
-      </Routes>
-    </div>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={location.pathname}
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{ duration: 0.9 }}
+        className="main-container"
+        variants={{
+          initialState: {
+            opacity: 0,
+            // clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+            clipPath: "polygon(0 0, 0 100%, 0% 100%, 0% 0%)",
+            height: 0,
+          },
+          animateState: {
+            opacity: 1,
+            // clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+            height: "100%",
+          },
+          exitState: { clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)" },
+        }}
+      >
+        <Routes location={location} key={location.pathname}>
+          <Route index element={<Home />} />
+          <Route path="/register" element={<Registration />} />
+        </Routes>
+      </motion.div>
+    </AnimatePresence>
   );
 }
--- src/routes.jsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Registration from "./pages/Registration";
-
-export const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/register", element: <Registration /> },
-]);
