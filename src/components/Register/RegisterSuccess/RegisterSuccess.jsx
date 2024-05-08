e097305b03fb687b1acc2285f4b35b33c91c32a9
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-05-09 00:53:43 +0530

    Add animation to RegisterSuccess Component

--- src/components/Register/RegisterSuccess/RegisterSuccess.jsx
+++ src/components/Register/RegisterSuccess/RegisterSuccess.jsx
@@ -2,27 +2,46 @@ import { useEffect, useState } from "react";
 import SuccessLogo from "../../../assets/svg/success.svg";
 import styles from "./RegisterSuccess.module.css";
 import { useNavigate } from "react-router";
+import { motion } from "framer-motion";
 
 export default function RegisterSuccess() {
-    const [timer, setTimer] = useState(5);
-    const navigate = useNavigate();
-  
-    useEffect(() => {
-      if (timer > 0) {
-        const interval = setInterval(() => {
-          setTimer((prevCount) => prevCount - 1);
-        }, 1000);
-  
-        return () => clearInterval(interval);
-      }
-      else if (timer === 0) {
-        navigate("/");
-      }
-    }, [timer,navigate]);
-  
+  const [timer, setTimer] = useState(5);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (timer > 0) {
+      const interval = setInterval(() => {
+        setTimer((prevCount) => prevCount - 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    } else if (timer === 0) {
+      navigate("/");
+    }
+  }, [timer, navigate]);
+
   return (
-    <section className={styles.success_info_container}>
-      <header >
+    <motion.section
+      className={styles.success_info_container}
+      initial="initialState"
+      animate="animateState"
+      exit="exitState"
+      transition={{ duration: 1 }}
+      variants={{
+        initialState: {
+          opacity: 0,
+          clipPath: "polygon(0 0, 0 100%, 0% 100%, 0% 0%)",
+          height: 0,
+        },
+        animateState: {
+          opacity: 1,
+          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+          height: "100%",
+        },
+        exitState: { clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)" },
+      }}
+    >
+      <header>
         <img src={SuccessLogo} alt="Green Checkmark Logo" />
         <h3>Success Submitted</h3>
         <h1>Congratulations</h1>
@@ -31,7 +50,12 @@ export default function RegisterSuccess() {
           your information and reach out to your shortly with updates
         </p>
       </header>
-      <p className={styles.redirection}>Redirecting you to Homepage in <span>{timer} Second{timer===1?"":"s"}</span></p>
-    </section>
+      <p className={styles.redirection}>
+        Redirecting you to Homepage in{" "}
+        <span>
+          {timer} Second{timer === 1 ? "" : "s"}
+        </span>
+      </p>
+    </motion.section>
   );
 }
